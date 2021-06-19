54eb85ed014c6ff5ab34b3d3efd774c35850ed13
Morum Jaswanth <mjaswanthjp@gmail.com>
2021-06-19 14:54:18 +0530

    Added two files

--- query4.sql
+++ query4.sql
@@ -1,2 +1,2 @@
-select s.name from subjects s, books b , books_subject bs
+select s.name from subjects s, books b , books_subjects bs
 where bs.book=b.id and bs.subject=s.id and b.title='Atomic Habits';
